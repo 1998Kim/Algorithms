3372e6fa97f9909d6a70e52ec4cce012481d48b6
Kim soohwan <97665034+1998Kim@users.noreply.github.com>
2023-06-09 17:28:46 +0900

    [level 2] Title: 피보나치 수, Time: 21.64 ms, Memory: 13.9 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
@@ -1,6 +1,14 @@
-# DP로 풀이
+# DP 풀이
 def solution(n):
     answer = [0, 1]
     for idx in range(2, n+1):
         answer.append((answer[idx-2] + answer[idx-1]) % 1234567)
     return answer[-1]
+
+# 재귀함수 풀이
+# def factorial(n):
+#     pass
+
+# def solution(n):
+#     answer = 0
+#     return answer
