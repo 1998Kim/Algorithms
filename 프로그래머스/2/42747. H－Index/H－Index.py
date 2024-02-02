34b2fd8e226ee1cbf689fff7399566ab489ae896
Kim soohwan <97665034+1998Kim@users.noreply.github.com>
2024-02-02 23:49:51 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -0,0 +1,8 @@
+def solution(citations):
+    answer = 0
+    citations.sort(reverse=True)
+    
+    for index in range(len(citations)):
+        if citations[index] >= (index+1):
+            answer = index + 1
+    return answer
