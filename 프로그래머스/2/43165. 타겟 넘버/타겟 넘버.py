2b36fca3960eae6ecf2c1771bfc361cac9d59998
Kim soohwan <97665034+1998Kim@users.noreply.github.com>
2024-02-12 14:53:04 +0900

    [level 2] Title: 타겟 넘버, Time: 2.86 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,18 +1,36 @@
-def solution(numbers, target):
-    answer = 0
-    results = [0]
+# BFS
+# def solution(numbers, target):
+#     answer = 0
+#     results = [0]
     
-    for num in numbers:
-        temp = []
+#     for num in numbers:
+#         temp = []
         
-        for result in results:
-            temp.append(result + num)
-            temp.append(result - num)
+#         for result in results:
+#             temp.append(result + num)
+#             temp.append(result - num)
             
-        results = temp
+#         results = temp
         
-    for result in results:
-        if result == target:
-            answer += 1
+#     for result in results:
+#         if result == target:
+#             answer += 1
     
+#     return answer
+
+# DFS
+answer = 0
+
+def dfs(numbers, target, depth, value):
+    global answer
+
+    if depth == len(numbers):
+        if value == target:
+            answer += 1
+        return
+    dfs(numbers, target, depth +1, value + numbers[depth])
+    dfs(numbers, target, depth +1, value - numbers[depth])
+
+def solution(numbers, target):
+    dfs(numbers, target, 0, 0)
     return answer
