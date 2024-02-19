514ef8d013000e4c63c3d3d6291d1b350f84c14f
Kim soohwan <97665034+1998Kim@users.noreply.github.com>
2024-02-19 19:23:56 +0900

    [level 2] Title: 순위 검색, Time: 1025.28 ms, Memory: 61.4 MB -BaekjoonHub

--- 프로그래머스/2/72412. 순위 검색/순위 검색.py
+++ 프로그래머스/2/72412. 순위 검색/순위 검색.py
@@ -3,39 +3,39 @@ def solution(info, query):
     
     info_dict = {}
     for language in ['cpp', 'java', 'python', '-']:
-        for job in ['backend', 'frontend', '-' ]:
+        for work in ['backend', 'frontend', '-']:
             for carrer in ['junior', 'senior', '-']:
                 for food in ['chicken', 'pizza', '-']:
-                    info_dict[language + job + carrer + food] = []
-                    
-    for tmp in info:
-        tmp = tmp.split(' ')
-        for language in [tmp[0], '-']:
-            for job in [tmp[1], '-' ]:
-                for carrer in [tmp[2], '-']:
-                    for food in [tmp[3], '-']:
-                        info_dict[language + job + carrer + food].append(int(tmp[4]))
+                    info_dict[language + '/' + work + '/' + carrer + '/' + food] = []
+    for temp in info:
+        temp = temp.split()
+        for language in [temp[0], '-']:
+            for work in [temp[1], '-']:
+                for carrer in [temp[2], '-']:
+                    for food in [temp[3], '-']:
+                        info_dict[language + '/' + work + '/' + carrer + '/' + food].append(int(temp[4]))
     
     for key in info_dict.keys():
-        info_dict[key].sort()
+        info_dict[key].sort()    
     
     for que in query:
-        que = que.replace(' and ', '').split(' ')
+        que = que.replace(' and ', '/').split(' ')
         condition = que[0]
-        score = int(que[-1])
-        
+        que_score = int(que[-1])
+
         info_scores = info_dict[condition]
+        
         left_index, right_index = 0, len(info_scores) - 1
-        temp_index = len(info_scores)
+        tmp_index = len(info_scores)
         
         while left_index <= right_index:
             mid_index = (left_index + right_index) // 2
             
-            if score <= info_scores[mid_index]:
-                temp_index = mid_index
+            if que_score <= info_scores[mid_index]:
+                tmp_index = mid_index
                 right_index = mid_index - 1
-            else:
+            elif que_score > info_scores[mid_index]:
                 left_index = mid_index + 1
-        
-        answer.append(len(info_scores) - temp_index)
+            
+        answer.append(len(info_scores) - tmp_index)
     return answer
