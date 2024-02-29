d10f9d052dc79fdce6db261f4162a52da6820580
Kim soohwan <97665034+1998Kim@users.noreply.github.com>
2024-02-29 16:29:58 +0900

    [level 2] Title: 롤케이크 자르기, Time: 776.03 ms, Memory: 51.3 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -10,19 +10,18 @@
 #     return answer
 from collections import Counter
 
-
 def solution(topping):
     answer = 0
+    
     up = Counter(topping)
     down = set()
-    
     for t in topping:
         up[t] -= 1
         down.add(t)
         
         if up[t] == 0:
             up.pop(t)
-
         if len(up.keys()) == len(down):
             answer += 1
+        
     return answer
